8074c60c5917bb286f438821a44e817c6f95efc8
JinseongHwang <eddy5360@naver.com>
2021-04-21 22:26:26 +0900

    nm

---
+++ 04-NumpyMatrix-2.py
@@ -0,0 +1,14 @@
+from numpy import *
+
+# 3 by 3 행렬인데, 모든 원소가 1인 행렬
+aones = ones((3,3))
+
+# 3 by 3 행렬인데, 모든 원소가 0인 행렬
+azeros = zeros((3,3))
+
+# 3 by 3 행렬인데, 대각 행렬
+aeye = eye(3)
+
+print(aones)
+print(azeros)
+print(aeye)
